caa5980ea5a0edc5ebf081b7bcf51ff66c737552
EnapsTer <andreshah1@yandex.ru>
2021-07-17 13:19:57 +0300

    change error message in day05 ex00

--- Day05/ex00/main.cpp
+++ Day05/ex00/main.cpp
@@ -7,44 +7,38 @@
 #include <iostream>
 
 int main() {
-  Bureaucrat *b;
 
   try {
-    b = new Bureaucrat("Garik", 190);
+    Bureaucrat b("Garik", 190);
     std::cout << b;
   } catch (std::exception & e) {
     std::cerr << "Exception: " << e.what() << std::endl;
   }
 
-  delete b;
-
   try {
-    b = new Bureaucrat("Garik", 130);
-    Bureaucrat o = *b;
+    Bureaucrat b("Garik", 130);
+    Bureaucrat o = b;
     std::cout << o;
   } catch (std::exception & e) {
     std::cerr << "Exception: " << e.what() << std::endl;
   }
 
-  delete b;
 
   try {
-    b = new Bureaucrat("Garik", 4);
+    Bureaucrat b("Garik", 4);
     for (int i = 0; i < 8; ++i)
-      b->Upgrade();
+      b.Upgrade();
   } catch (std::exception & e) {
     std::cerr << "Exception: " << e.what() << std::endl;
   }
 
-  delete b;
-
   try {
-    b = new Bureaucrat("Garik", 154);
+    Bureaucrat b("Garik", 154);
     for (int i = 0; i < 8; ++i)
-      b->Downgrade();
+      b.Downgrade();
   } catch (std::exception & e) {
     std::cerr << "Exception: " << e.what() << std::endl;
   }
- // TODO test leak
+
   return 0;
 }
