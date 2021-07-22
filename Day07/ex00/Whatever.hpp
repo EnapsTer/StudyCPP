ddbe5f067fec5c4ab837e88b65bd5d38117d73d7
EnapsTer <andreshah1@yandex.ru>
2021-07-22 13:39:43 +0300

    Add ex00 for Module07

---
+++ Day07/ex00/Whatever.hpp
@@ -0,0 +1,26 @@
+//
+// Created by Андре Шагиджанян on 22.07.2021.
+//
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+
+void swap(T &a, T &b) {
+  T c = a;
+  a = b;
+  b = c;
+}
+
+template <typename T>
+const T &min(const T &a, const T &b) {
+  return a < b ? a : b;
+}
+
+template <typename T>
+const T &max(const T &a, const T &b) {
+  return a > b ? a : b;
+}
+
+#endif
