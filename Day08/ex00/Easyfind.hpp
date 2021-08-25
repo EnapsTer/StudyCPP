9bb8055ef581f19f27f02559cbc7410d0294fff4
EnapsTer <andreshah1@yandex.ru>
2021-08-25 19:14:37 +0300

    Add ex00 to Day08

--- Day08/ex00/Easyfind.hpp
+++ Day08/ex00/Easyfind.hpp
@@ -7,17 +7,17 @@
 
 #include <string>
 
-template<typename T>
-typename T::const_iterator Easyfind(const T &container, const int element) {
-  class NotFoundException : std::exception {
-   private:
-    std::string m_error_;
-   public:
-    NotFoundException() : m_error_(std::string("Element not found.")){}
-    virtual ~NotFoundException() throw() {}
-	const char* what() const throw() {return m_error_.c_str()};
-  };
+class NotFoundException : public std::exception {
+ private:
+  std::string m_error_;
+ public:
+  NotFoundException() throw() : m_error_(std::string("Element not found.")){}
+  virtual ~NotFoundException() throw() {}
+  const char* what() const throw() {return m_error_.c_str();};
+};
 
+template<typename T>
+typename T::const_iterator Easyfind(T &container, const int element) {
   typename T::iterator iter;
   iter = std::find(container.begin(), container.end(), element);
   if (iter == container.end())
--- Day08/ex00/main.cpp
+++ Day08/ex00/main.cpp
@@ -9,8 +9,35 @@
 
 int main() {
   std::list<int> list(5);
-  for (int i = 0; i < static_cast<int>(list.size()); ++i) {
+  std::cout << "list - ";
+  for (int i = 0; i < 5; ++i) {
+    list.push_back(i * i);
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+  try {
+    std::cout << "Find 4. Answer: " << *Easyfind(list, 4) << std::endl;
+    std::cout << "Find 1. Answer: " << *Easyfind(list, 1) << std::endl;
+    std::cout << "Find 5. Answer: " << *Easyfind(list, 5) << std::endl;
+  } catch (std::exception & e) {
+    std::cerr << "Exception: " << e.what();
+  }
+
+  std::cout << std::endl;
+
+  std::vector<int> vector(5);
+  std::cout << "vector - ";
+  for (int i = 0; i < 5; ++i) {
     list.push_back(i);
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+  try {
+    std::cout << "Find 3. Answer: " << *Easyfind(list, 3) << std::endl;
+    std::cout << "Find 1. Answer: " << *Easyfind(list, 2) << std::endl;
+    std::cout << "Find 123. Answer: " << *Easyfind(list, 123) << std::endl;
+  } catch (std::exception & e) {
+    std::cerr << "Exception: " << e.what();
   }
 
 }
